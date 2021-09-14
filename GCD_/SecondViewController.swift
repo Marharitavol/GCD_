7621b418ee9427e60b89b428fb3a1cd1ed4cb3d4
Marharitavol <marharitavol@gmail.com>
2021-09-14 15:54:30 +0300

    - Fetch image from url; - added activity indicator

---
+++ GCD_/SecondViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.swift
+//  GCD_
+//
+//  Created by Rita on 14.09.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            activityIndicator.startAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://www.meme-arsenal.com/memes/22ad0506c6d7c05678072c74e17eb175.jpg")
+        activityIndicator.isHidden  = false
+        activityIndicator.startAnimating()
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
+        }
+    }
